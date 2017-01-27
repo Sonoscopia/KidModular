97276d9024029762c02eb582d7335701c217cd9b
Tiago Ângelo <tiagoangelo@Tiago-Angelos-MacBook-Pro-15.local>
2017-01-27 16:11:22 +0000

    Added encoder example

---
+++ examples/Encoder_example/Encoder_example.ino
@@ -0,0 +1,41 @@
+/* Encoder Library - Basic Example
+ * http://www.pjrc.com/teensy/td_libs_Encoder.html
+ *
+ * This example code is in the public domain.
+ */
+
+#include <Encoder.h>
+#define BUTTONPIN 7
+// Change these two numbers to the pins connected to your encoder.
+//   Best Performance: both pins have interrupt capability
+//   Good Performance: only the first pin has interrupt capability
+//   Low Performance:  neither pin has interrupt capability
+Encoder myEnc(5, 6);
+//   avoid using pins with LEDs attached
+
+byte toggle = 0; 
+byte button, _button;
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Basic Encoder Test:");
+  pinMode(BUTTONPIN, INPUT_PULLUP);
+}
+
+long oldPosition  = -999;
+
+void loop() {
+  long newPosition = myEnc.read();
+  if (newPosition != oldPosition) {
+    oldPosition = newPosition;
+    Serial.println(newPosition);
+  }
+  
+  button = digitalRead(BUTTONPIN);
+  if(button < 1 && button != _button){
+    toggle = !toggle;
+    Serial.println(toggle);
+  } 
+  _button = button;
+}
