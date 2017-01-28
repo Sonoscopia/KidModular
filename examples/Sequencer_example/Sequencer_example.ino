d1a469754395d4d7e8858dfe7258aea5201793bc
Tiago Ângelo <tiagoangelo@Tiago-Angelos-MacBook-Pro-15.local>
2017-01-28 19:39:35 +0000

    Added Sequencer example

---
+++ examples/Sequencer_example/Sequencer_example.ino
@@ -0,0 +1,40 @@
+#define V 32
+#define LED 5
+#define POT 0 
+#define GATEOUT 13
+
+byte bpm = 120;
+int stepTime; 
+byte curStep = 0;
+int playhead = 1;
+byte dur[] = {50, 50, 50, 50};
+byte numSteps = 4;
+
+void setup(){
+  stepTime = 60000.f / bpm;
+  pinMode(LED, OUTPUT);
+  pinMode(13, OUTPUT);
+}
+
+void loop(){
+  runSequence();
+  delayMicroseconds(750); // in a genuine Arduino with a 16MHz crystal oscillator the delay should be 1ms
+  // but on fake Arduinos with 12MHz oscillator the delay should be 0.75ms to have an accurrate clock running once every millisecond
+}
+
+void runSequence(){
+  playhead++;
+  if(playhead > numSteps * stepTime) playhead = 1; // reset playhead
+  curStep = playhead / stepTime; // increment current step
+  // Gate CV output 
+ if(playhead % stepTime < stepTime * dur[curStep] / 100.f){
+   digitalWrite(GATEOUT, HIGH);
+   analogWrite(curStep+LED, 255-V);
+ } 
+ else{
+   digitalWrite(GATEOUT, LOW);
+   analogWrite(curStep+LED, 255-LOW);
+ }
+ 
+ dur[0] = analogRead(0) / 1024.f * 100;
+}
