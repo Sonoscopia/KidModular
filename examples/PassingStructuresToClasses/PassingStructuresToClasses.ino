4a96455132955facff672db9a358db9379d91158
p1nh0 <p1nh0.c0d1ng@gmail.com>
2018-03-16 09:33:49 +0000

    improved example
    now able to reference struct object to class object

--- examples/PassingStructuresToClasses/PassingStructuresToClasses.ino
+++ examples/PassingStructuresToClasses/PassingStructuresToClasses.ino
@@ -6,7 +6,12 @@ aClass c;
 
 void setup() {
   Serial.begin(9600);
-  c.printColor(&color);
+  //c.printColor(&color);
+  c.passStruct(&color);
+  c.randomColor();
+  Serial.println(color.r);
+  Serial.println(color.g);
+  Serial.println(color.b);
 }
 
 void loop() {
